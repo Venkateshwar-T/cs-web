991c5034556f554bda8840b476363cc4e20c4032
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:01:19 +0000

    good but you reduced the width of left side recatangle. Undo that

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center">
-        <div className="w-1/4 h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="h-4 bg-[#5d2b79] w-3/4 rounded-l-full mt-4 shadow-md"></div>
+        <div className="w-1/2 h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
+        <div className="h-4 bg-[#5d2b79] w-1/2 rounded-l-full mt-4 shadow-md"></div>
       </div>
     </main>
   );
