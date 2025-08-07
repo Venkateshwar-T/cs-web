f4292b2235a278797d97eae5d48fbbcf08496339
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:40:54 +0000

    reduce the height of the golden rectangle just by a little

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <div className="flex items-center">
         <div className="w-[45%] h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="relative h-12 bg-yellow-400 flex-grow rounded-l-full mt-4 shadow-md -ml-12 flex items-center">
+        <div className="relative h-10 bg-yellow-400 flex-grow rounded-l-full mt-4 shadow-md -ml-12 flex items-center">
           <div className="ml-3 w-6 h-6 bg-white rounded-full"></div>
         </div>
       </div>
