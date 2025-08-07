878c55f864d15573e7181254f8891c928551b0a4
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:30:09 +0000

    can you also increase the height of the purple recatngle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center">
-        <div className="w-[45%] h-20 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="relative h-10 bg-yellow-400 w-[55%] rounded-l-full mt-4 shadow-md -ml-12 flex items-center">
+        <div className="w-[45%] h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
+        <div className="relative h-12 bg-yellow-400 w-[55%] rounded-l-full mt-4 shadow-md -ml-16 flex items-center">
           <div className="ml-3 w-6 h-6 bg-white rounded-full"></div>
         </div>
       </div>
