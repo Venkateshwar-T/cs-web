69d7904ad9c37f423a7dc02dc46ed8e9aa586649
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:03:04 +0000

    by 20 px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center">
-        <div className="w-1/3 h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="h-4 bg-[#5d2b79] w-2/3 rounded-l-full mt-4 shadow-md"></div>
+        <div className="w-[35%] h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
+        <div className="h-4 bg-[#5d2b79] w-[65%] rounded-l-full mt-4 shadow-md"></div>
       </div>
     </main>
   );
