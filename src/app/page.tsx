c87a1d9d858737fef669ed99dd796dfa34205faf
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:17:03 +0000

    just a little more left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <div className="flex items-center">
         <div className="w-[40%] h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="h-10 bg-yellow-500 w-[60%] rounded-l-full mt-4 shadow-md -ml-8"></div>
+        <div className="h-10 bg-yellow-500 w-[60%] rounded-l-full mt-4 shadow-md -ml-10"></div>
       </div>
     </main>
   );
