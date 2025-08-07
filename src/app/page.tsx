88286b3fd761402c0163fa52fca9a2af3d2cadbe
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:29:13 +0000

    ok cool. now make sure the circle fits properly in the rectangle (equal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main>
       <div className="flex items-center">
         <div className="w-[45%] h-20 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="relative h-8 bg-yellow-400 w-[60%] rounded-l-full mt-4 shadow-md -ml-20">
-          <div className="absolute left-2 top-1/2 -translate-y-1/2 w-6 h-6 bg-white rounded-full"></div>
+        <div className="relative h-10 bg-yellow-400 w-[55%] rounded-l-full mt-4 shadow-md -ml-12 flex items-center">
+          <div className="ml-3 w-6 h-6 bg-white rounded-full"></div>
         </div>
       </div>
     </main>
