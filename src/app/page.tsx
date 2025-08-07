64f6d11889d38d92f96f3b735590aec78b5ec009
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:25:04 +0000

    good now reduce its width from the left side just by a little

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <div className="flex items-center">
         <div className="w-[45%] h-20 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="h-8 bg-yellow-400 w-[65%] rounded-l-full mt-4 shadow-md -ml-16"></div>
+        <div className="h-8 bg-yellow-400 w-[65%] rounded-l-full mt-4 shadow-md -ml-12"></div>
       </div>
     </main>
   );
