62cc290c3207b79d7e9db3a831f427716d63b8bd
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 11:57:47 +0000

    cool. Now give a little gap from the top very little and also make the r

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="w-1/2 h-24 bg-[#5d2b79] rounded-r-2xl shadow-md"></div>
+      <div className="w-1/2 h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
     </main>
   );
 }
