fa188083b2470123c8407fcc1b7c794a0c7e380c
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:00:12 +0000

    Nice bro. Now add a small strip like long rectangle from the extreme rig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <div className="w-1/2 h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
+      <div className="flex items-center">
+        <div className="w-1/4 h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
+        <div className="h-4 bg-[#5d2b79] w-3/4 rounded-l-full mt-4 shadow-md"></div>
+      </div>
     </main>
   );
 }
