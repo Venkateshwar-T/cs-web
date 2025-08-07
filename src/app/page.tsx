597f97364da356d27653488bd4100a6d9fcb4d81
Venkateshwar <venkattiwari42@gmail.com>
2025-08-07 12:22:46 +0000

    reduced the height of the golden rectangle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center">
-        <div className="w-[45%] h-24 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
-        <div className="h-24 bg-yellow-400 w-[65%] rounded-l-full mt-4 shadow-md -ml-16"></div>
+        <div className="w-[45%] h-20 bg-[#5d2b79] rounded-r-full shadow-md mt-4"></div>
+        <div className="h-20 bg-yellow-400 w-[65%] rounded-l-full mt-4 shadow-md -ml-16"></div>
       </div>
     </main>
   );
