d70c5a0c65d89076b207021873da6b4d2c989f71
Venkateshwar <venkattiwari42@gmail.com>
2025-08-09 17:10:04 +0000

    where's the purple part?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome!</h1>
     </main>
   );
 }
