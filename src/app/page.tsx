82b3a07ea710006d55a62337885ec45034fcbc15
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 16:19:33 +0000

    make sure its alignment is from starting of the box instead of the cente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="flex-grow pt-72 overflow-hidden">
         <div className="bg-white/50 h-full rounded-t-2xl mx-8 md:mx-32 p-8 overflow-y-auto no-scrollbar">
           <h2 className="text-2xl font-bold text-white mb-8">Explore Categories</h2>
-          <div className="flex flex-wrap justify-center gap-8">
+          <div className="flex flex-wrap justify-start gap-8">
             <div className="bg-card rounded-lg w-48 h-64 flex-shrink-0"></div>
             <div className="bg-card rounded-lg w-48 h-64 flex-shrink-0"></div>
             <div className="bg-card rounded-lg w-48 h-64 flex-shrink-0"></div>
