a3b34d4b5e10a825ae00a8b6c2e61601108b831e
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 07:26:46 +0000

    give a little more padding from left and right and also from top and als

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,9 +14,9 @@ export function Header() {
   ];
 
   return (
-    <header className="fixed top-0 z-50 w-full bg-transparent pt-4">
-      <div className="container flex h-20 max-w-screen-2xl items-center justify-between px-4 md:px-6">
-        <div className="flex items-center gap-4">
+    <header className="fixed top-0 z-50 w-full bg-transparent pt-6">
+      <div className="container flex h-20 max-w-screen-2xl items-center justify-between px-8 md:px-12">
+        <div className="flex items-center gap-8">
           <Link href="/" className="flex items-center gap-2">
             <Image 
               src="/Choco Smiley Logo.png" 
