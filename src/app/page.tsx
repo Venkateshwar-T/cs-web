eb9fd777184cf2f48ed1d17715ff079fe5b97c35
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 19:06:25 +0000

    make it 34

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       <Header onSearchSubmit={handleSearchSubmit} onSearchActiveChange={setIsSearchActive} />
       <main className={cn(
         "flex-grow overflow-hidden flex transition-all duration-500",
-        isSearchActive ? 'pt-32' : 'pt-72',
+        isSearchActive ? 'pt-36' : 'pt-72',
         isLoading ? 'items-center justify-center' : 'items-start'
       )}>
         {!isSearchActive && (
