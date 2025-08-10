6f2571137eb639cbb2453d3766d868456c0e01d1
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 19:19:52 +0000

    can you make corners of both the rectangle as more rounded like 50%

--- src/components/search-results-details.tsx
+++ src/components/search-results-details.tsx
@@ -5,7 +5,7 @@ interface SearchResultsDetailsProps {
 
 export function SearchResultsDetails({ query }: SearchResultsDetailsProps) {
   return (
-    <div className="bg-white/50 h-full flex-grow rounded-tl-2xl rounded-tr-2xl p-8 ml-8 mr-8">
+    <div className="bg-white/50 h-full flex-grow rounded-t-3xl p-8 ml-8 mr-8">
       <h2 className="text-xl text-white">
         Showing results for <span className="italic text-custom-gold">{query}</span>
       </h2>
