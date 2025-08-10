fde9700225300322503fddde1d2964059ca4b5f3
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 09:17:11 +0000

    cool but i wan the white box to be upto the screen's bottom only. no scr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import { Header } from "@/components/header";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="pt-56">
-        <div className="bg-white h-96 rounded-t-2xl mx-4">
+      <main className="flex-grow pt-56">
+        <div className="bg-white h-full rounded-t-2xl mx-4">
           {/* Content for the white box can go here */}
         </div>
       </main>
