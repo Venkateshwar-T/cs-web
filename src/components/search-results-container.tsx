3574b69c0da74a1d4e9a3097e1868283d271b9f6
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 18:49:39 +0000

    20% sorry

--- src/components/search-results-container.tsx
+++ src/components/search-results-container.tsx
@@ -1,7 +1,7 @@
 
 export function SearchResultsContainer() {
   return (
-    <div className="bg-white/50 h-full w-2/5 rounded-tr-2xl p-8">
+    <div className="bg-white/50 h-full w-1/5 rounded-tr-2xl p-8">
       {/* Content for the search results will go here */}
     </div>
   );
