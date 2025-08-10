af75731b64d20fcde88c7c863408429d67e70bd9
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 19:18:00 +0000

    make it less bold

--- src/components/search-results-details.tsx
+++ src/components/search-results-details.tsx
@@ -7,7 +7,7 @@ export function SearchResultsDetails({ query }: SearchResultsDetailsProps) {
   return (
     <div className="bg-white/50 h-full flex-grow rounded-tl-2xl rounded-tr-2xl p-8 ml-8 mr-8">
       <h2 className="text-xl text-white">
-        Showing results for <span className="italic font-bold text-custom-gold">{query}</span>
+        Showing results for <span className="italic text-custom-gold">{query}</span>
       </h2>
     </div>
   );
