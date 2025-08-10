52339d9445fa7b39848d2215e77d8c81bf63d55b
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 18:14:33 +0000

    When i type something and click enter the search bar goes upto the Navig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,9 +17,15 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen">
       <Header isSearchActive={isSearchActive} onSearchSubmit={handleSearchSubmit} />
-      {!isSearchActive && (
+      {!isSearchActive ? (
         <main className="flex-grow pt-72 overflow-hidden transition-opacity duration-500 opacity-100 flex">
           <ExploreCategories />
+        </main>
+      ) : (
+        <main className="flex-grow pt-48">
+            <div className="bg-white h-full mx-8 md:mx-32 rounded-t-2xl p-8">
+                {/* Search results will go here */}
+            </div>
         </main>
       )}
     </div>
