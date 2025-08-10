bf9da426722307226ebd38842a135317d96ba1ec
Venkateshwar <venkattiwari42@gmail.com>
2025-08-10 09:14:46 +0000

    add a white rectangular box below the search bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import { Header } from "@/components/header";
 
 export default function Home() {
   return (
-    <>
+    <div className="relative min-h-screen">
       <Header />
-      <main className="flex items-center justify-center min-h-screen">
+      <main className="pt-48">
+        <div className="bg-white h-screen rounded-t-2xl mx-4">
+          {/* Content for the white box can go here */}
+        </div>
       </main>
-    </>
+    </div>
   );
 }
