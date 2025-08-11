412b5bf43cea89266f60f9e9983343d1a3680188
Venkateshwar <venkattiwari42@gmail.com>
2025-08-11 16:56:21 +0000

    Bro set the color as white and font as purple what are you doing. And wh

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -53,9 +53,8 @@ export function ProductCard() {
               {quantity === 0 ? (
                 <Button
                   size="sm"
-                  variant="outline"
                   onClick={handleAddToCart}
-                  className="w-full rounded-full uppercase border-2 border-custom-purple-dark text-custom-purple-dark hover:bg-custom-purple-dark hover:text-white"
+                  className="w-full rounded-full uppercase border-2 bg-transparent border-custom-purple-dark text-custom-purple-dark hover:bg-custom-purple-dark hover:text-white"
                 >
                   Add
                 </Button>
