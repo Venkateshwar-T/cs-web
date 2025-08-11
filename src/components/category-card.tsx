e9db98f5fd10cbfe15dd2ea31e0b788afdeb27d8
Venkateshwar <venkattiwari42@gmail.com>
2025-08-11 07:36:13 +0000

    this entire container is responsive but not the box in it. Make the box

--- src/components/category-card.tsx
+++ src/components/category-card.tsx
@@ -1,5 +1,5 @@
 export function CategoryCard() {
   return (
-    <div className="bg-card rounded-lg w-64 h-72 flex-shrink-0"></div>
+    <div className="bg-card rounded-lg w-48 h-56 sm:w-56 sm:h-64 md:w-64 md:h-72 flex-shrink-0"></div>
   );
 }
