55148826a5aa8639762dd986b05987ff1c3ea88f
Venkateshwar <venkattiwari42@gmail.com>
2025-08-12 07:37:04 +0000

    Cool you fixed it. But can you move it to a little right so that it touc

--- src/components/search-results-details.tsx
+++ src/components/search-results-details.tsx
@@ -14,7 +14,7 @@ export function SearchResultsDetails({ query, onAddToCart, cart }: SearchResults
 
   return (
     <div className="bg-white/20 h-full flex-grow rounded-t-[40px] p-8 ml-12 mr-8">
-      <div className="h-full overflow-y-auto custom-scrollbar -mr-4 pr-4">
+      <div className="h-full overflow-y-auto custom-scrollbar">
         <h2 className="text-xl text-white mb-6">
           Showing results for <span className="italic text-custom-gold">{query}</span>
         </h2>
