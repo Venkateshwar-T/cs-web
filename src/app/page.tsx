c798372b5aad8217293e74252976b2862d8cf2b0
Venkateshwar <venkattiwari42@gmail.com>
2025-08-13 11:26:08 +0000

    yaeh that better now. Now remove the loading icon after searching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import { Header } from "@/components/header";
 import { ExploreCategories } from '@/components/explore-categories';
 import { cn } from '@/lib/utils';
-import { Loader } from '@/components/loader';
 import { FilterContainer } from '@/components/filter-container';
 import { SearchResultsDetails } from '@/components/search-results-details';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   const [isSearchActive, setIsSearchActive] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [cart, setCart] = useState<Record<string, number>>({});
   const [cartMessage, setCartMessage] = useState('');
@@ -22,7 +20,6 @@ export default function Home() {
   const handleSearchSubmit = (query: string) => {
     setSearchQuery(query);
     setIsSearchActive(true);
-    setIsLoading(true);
   };
   
   const handleAddToCart = (productName: string, quantity: number) => {
@@ -45,30 +42,19 @@ export default function Home() {
 
   const totalQuantity = Object.values(cart).reduce((acc, cur) => acc + cur, 0);
 
-  useEffect(() => {
-    if (isLoading) {
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, [isLoading]);
-
   return (
     <div className="flex flex-col h-screen">
       <Header onSearchSubmit={handleSearchSubmit} onSearchActiveChange={setIsSearchActive} />
       <main className={cn(
-        "flex-grow overflow-hidden flex transition-all duration-500 relative",
-        isSearchActive ? 'pt-36' : 'pt-72',
-        isLoading ? 'items-center justify-center' : 'items-start'
+        "flex-grow overflow-hidden flex transition-all duration-500 relative items-start",
+        isSearchActive ? 'pt-36' : 'pt-72'
       )}>
         {!isSearchActive && (
           <div className={cn("transition-opacity duration-500 w-full", isSearchActive ? 'opacity-0' : 'opacity-100 h-full')}>
             <ExploreCategories />
           </div>
         )}
-        {isLoading && <Loader />}
-        {isSearchActive && !isLoading && (
+        {isSearchActive && (
           <>
             <div className="flex w-full h-full">
               <FilterContainer />
