7142f13a058116c5146d2e738ef6467915c2c17c
Venkateshwar <venkattiwari42@gmail.com>
2025-08-14 07:37:57 +0000

    I see that the cards are cut from the top and bottom a little. why?

--- src/components/flavours-section.tsx
+++ src/components/flavours-section.tsx
@@ -61,7 +61,7 @@ export function FlavoursSection({ onAddToCart, cart }: FlavoursSectionProps) {
   };
 
   return (
-    <div className="bg-white/20 rounded-[40px] p-4 h-full flex flex-col relative">
+    <div className="bg-white/20 rounded-[40px] p-2 h-full flex flex-col relative">
       <SectionTitle className="pl-0 mb-2 text-lg">Flavours & Fillings</SectionTitle>
       
       {canScrollLeft && (
