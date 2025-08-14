d08b6ef62e5ac4911583e1f6534cc2feed1f9885
Venkateshwar <venkattiwari42@gmail.com>
2025-08-14 18:37:35 +0000

    can you make it really quick

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -139,7 +139,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'slide-up-fade-in': 'slide-up-fade-in 0.5s ease-out forwards',
+        'slide-up-fade-in': 'slide-up-fade-in 0.2s ease-out forwards',
         'fade-in': 'fade-in 0.5s ease-out forwards',
         'slide-in-from-left': 'slide-in-from-left 0.5s ease-out forwards',
         'slide-in-from-right': 'slide-in-from-right 0.5s ease-out forwards',
