45e0fded0c637f26ba4ba4619a438ec41ff77246
Venkateshwar <venkattiwari42@gmail.com>
2025-08-14 10:35:29 +0000

    now make the red container transparent. and make the cards #9A7DAB this

--- src/components/flavours-section.tsx
+++ src/components/flavours-section.tsx
@@ -67,7 +67,7 @@ export function FlavoursSection({ onAddToCart, cart }: FlavoursSectionProps) {
   };
 
   return (
-    <div className="bg-white/20 rounded-[40px] p-2 h-full flex flex-col relative">
+    <div className="bg-[#5D2B79] rounded-[40px] p-2 h-full flex flex-col relative">
       <SectionTitle className="pl-4 mb-2 text-lg">Flavours & Fillings</SectionTitle>
       
       {canScrollLeft && (
