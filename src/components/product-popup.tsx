3419b501cb53fe06deffa5e6ff7a8ed03157677a
Venkateshwar <venkattiwari42@gmail.com>
2025-08-14 06:25:28 +0000

    can you reduce green one too because the red one is currently bend the g

--- src/components/product-popup.tsx
+++ src/components/product-popup.tsx
@@ -32,17 +32,17 @@ export function ProductPopup({ product, onClose, onAddToCart, cart }: ProductPop
       
       {/* The content area of the popup is now a flex container for the three boxes */}
       <div className="flex flex-row flex-grow h-full gap-4">
-        {/* Left Column (1/3 width), which contains the top and bottom boxes */}
-        <div className="w-1/3 flex flex-col gap-4 h-full">
-          {/* 1. Top Left Box (Blue) */}
-          <div className="bg-blue-500 h-1/2 rounded-lg"></div>
-          {/* 2. Bottom Left Box (Red) */}
-          <div className="bg-red-500 h-1/2 rounded-lg w-[120%]"></div>
+        {/* Left Column (now 40% width to contain the wider red box) */}
+        <div className="w-2/5 flex flex-col gap-4 h-full">
+          {/* 1. Top Left Box (Blue) - Width is set to be 1/3 of the total container */}
+          <div className="bg-blue-500 h-1/2 rounded-lg w-[83.33%]"></div>
+          {/* 2. Bottom Left Box (Red) - Fills the 40% width of this column */}
+          <div className="bg-red-500 h-1/2 rounded-lg w-full"></div>
         </div>
         
-        {/* Right Column (2/3 width) */}
+        {/* Right Column (now 60% width) */}
         {/* 3. Right Box (Green) */}
-        <div className="w-2/3 bg-green-500 h-full rounded-lg"></div>
+        <div className="w-3/5 bg-green-500 h-full rounded-lg"></div>
       </div>
     </div>
   );
