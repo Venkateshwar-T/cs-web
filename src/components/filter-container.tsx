f8c4961eeebc45c09941922cd8d6ac8b86c0ae0f
Venkateshwar <venkattiwari42@gmail.com>
2025-08-14 16:46:38 +0000

    can you add slide in animation to this from left side (for element <Filt

--- src/components/filter-container.tsx
+++ src/components/filter-container.tsx
@@ -1,7 +1,9 @@
 
+import { cn } from "@/lib/utils";
+
 export function FilterContainer() {
   return (
-    <div className="bg-[#5D2B79] h-full w-[17%] rounded-tr-[40px]">
+    <div className={cn("bg-[#5D2B79] h-full w-[17%] rounded-tr-[40px] animate-slide-in-from-left")} style={{ animationDuration: '0.5s' }}>
         <div className="bg-white/20 h-full w-full rounded-tr-[40px] p-8">
             {/* Content for the filter will go here */}
         </div>
