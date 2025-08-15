72fe3e96bff5a8b11c0f74cd4942b0395b42c3fc
Venkateshwar <venkattiwari42@gmail.com>
2025-08-15 07:40:38 +0000

    what no. Now it overflows in smaller width devices. MAke sure its respon

--- src/components/filter-container.tsx
+++ src/components/filter-container.tsx
@@ -6,9 +6,9 @@ export function FilterContainer() {
   return (
     <div className={cn("bg-[#5D2B79] h-full w-[17%] rounded-tr-[40px] animate-slide-in-from-left")} style={{ animationDuration: '0.5s' }}>
         <div className="bg-white/20 h-full w-full rounded-tr-[40px] p-8">
-            <div className="flex items-center text-white text-xl font-bold mb-6">
-                <SlidersHorizontal className="h-6 w-6 mr-3" />
-                <h2 className="font-poppins whitespace-nowrap">Filters & Sorting</h2>
+            <div className="flex items-center text-white font-bold mb-6 text-lg xl:text-xl">
+                <SlidersHorizontal className="h-6 w-6 mr-3 flex-shrink-0" />
+                <h2 className="font-poppins">Filters & Sorting</h2>
             </div>
             {/* Content for the filter will go here */}
         </div>
