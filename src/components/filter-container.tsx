f1297b10c49552ac855c00aa1d733e38baeb17e9
Venkateshwar <venkattiwari42@gmail.com>
2025-08-15 07:41:57 +0000

    reduce text size by a little

--- src/components/filter-container.tsx
+++ src/components/filter-container.tsx
@@ -6,7 +6,7 @@ export function FilterContainer() {
   return (
     <div className={cn("bg-[#5D2B79] h-full w-[17%] rounded-tr-[40px] animate-slide-in-from-left")} style={{ animationDuration: '0.5s' }}>
         <div className="bg-white/20 h-full w-full rounded-tr-[40px] p-8">
-            <div className="flex items-center text-white font-bold mb-6 text-lg xl:text-xl">
+            <div className="flex items-center text-white font-bold mb-6 text-lg">
                 <SlidersHorizontal className="h-6 w-6 mr-3 flex-shrink-0" />
                 <h2 className="font-poppins">Filters & Sorting</h2>
             </div>
