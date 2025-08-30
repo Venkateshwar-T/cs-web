66810d9587e5a272f20499989372d0b3530c0614
Venkateshwar <venkattiwari42@gmail.com>
2025-08-30 11:08:31 +0000

    Good. Now make the bottom radius also same as the top one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -215,8 +215,8 @@ export default function Home() {
           isSearchActive ? 'pt-36' : 'pt-72'
         )}>
           {isOrderConfirmed ? (
-            <div className="bg-[#5D2B79] rounded-t-[40px] mx-8 md:mx-32 animate-fade-in">
-              <div className="bg-white/20 rounded-t-[40px] py-6 px-12 h-[150vh]">
+            <div className="bg-[#5D2B79] rounded-[40px] mx-8 md:mx-32 animate-fade-in">
+              <div className="bg-white/20 rounded-[40px] py-6 px-12 h-[150vh]">
                 {/* Content will be added later */}
               </div>
             </div>
