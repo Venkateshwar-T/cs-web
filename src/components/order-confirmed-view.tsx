78f8e528960517a589f773593e6a8d52e68c1ed0
Venkateshwar <venkattiwari42@gmail.com>
2025-08-31 13:52:29 +0000

    reduced height of order-confirmed-box

--- src/components/order-confirmed-view.tsx
+++ src/components/order-confirmed-view.tsx
@@ -3,7 +3,7 @@
 export function OrderConfirmedView() {
   return (
     <div className="bg-[#5D2B79] rounded-[40px] mx-8 md:mx-32 animate-fade-in">
-      <div className="bg-white/20 rounded-[40px] py-6 px-12 h-[150vh]">
+      <div className="bg-white/20 rounded-[40px] py-6 px-12 h-[100vh]">
         {/* Content will be added later */}
       </div>
     </div>
