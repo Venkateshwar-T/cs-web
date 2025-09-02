abfa597fbdf948bbf8dc068ac5f2be664a7c4713
Venkateshwar <venkattiwari42@gmail.com>
2025-09-02 07:49:06 +0000

    good. but i noticed that the expanded search bar disappear when enquire

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -127,7 +127,7 @@ export function Header({
           {showAnimatedSearch && (
              <div className={cn(
                 "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
-                isEnquireOpen && "hidden"
+                isEnquireOpen && "opacity-50 pointer-events-none"
               )}>
                 <AnimatedSearchBar 
                     onSearchSubmit={handleAnimatedSearchSubmit}
