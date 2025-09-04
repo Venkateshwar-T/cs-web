1c29f3d92b7d293008e5ce598ac8b91edd5e48da
Venkateshwar <venkattiwari42@gmail.com>
2025-09-04 09:10:08 +0000

    Good. But also make the search result container to be pulled up and take

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -248,9 +248,9 @@ function SearchPageComponent() {
           />
         )}
         <main className={cn(
-          "flex-grow flex flex-col transition-all duration-500 relative min-h-0",
+          "flex-grow flex flex-col transition-all duration-300 relative min-h-0",
           "pb-16 md:pb-0",
-          isMobile ? "pt-16" : "pt-24 md:pt-36"
+          isMobile ? (isMobileHeaderVisible ? 'pt-16' : 'pt-0') : "pt-24 md:pt-36"
         )}>
            <SearchView
               filters={filters}
