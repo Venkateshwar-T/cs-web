f171d801aae44cd117305a6749ce8b476f64c374
Venkateshwar <venkattiwari42@gmail.com>
2025-09-04 07:00:12 +0000

    dude more lesss

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -75,7 +75,7 @@ export function Header({
           
           <div className="hidden md:flex flex-1">
              {activeView === 'search' ? (
-                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-md lg:max-w-xl xl:max-w-2xl px-4">
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-md px-4">
                   <AnimatedSearchBar 
                       onSearchSubmit={handleAnimatedSearchSubmit}
                       isExpanded={true}
