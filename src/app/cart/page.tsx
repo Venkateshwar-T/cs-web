ee4c7271acdf4753bd96abd4017301f325937f89
Venkateshwar <venkattiwari42@gmail.com>
2025-09-04 13:14:35 +0000

    Cool. Now increase the height of the container. but dont increase the he

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -119,7 +119,7 @@ export default function CartPage() {
               {isMobile ? (
                 <div className="flex-grow overflow-y-auto no-scrollbar">
                   <div className="p-4 flex flex-col flex-grow">
-                    <div className="bg-white/80 rounded-2xl overflow-y-auto no-scrollbar">
+                    <div className="bg-white/80 rounded-2xl overflow-y-auto no-scrollbar flex-grow">
                       {cartItems.map(([productName, quantity], index) => (
                          <MobileCartItemCard
                             key={productName}
