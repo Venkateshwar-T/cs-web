5e5073fc25db93d991f6f1fb05dffc5e2108187e
Venkateshwar <venkattiwari42@gmail.com>
2025-09-07 13:00:20 +0000

    hey the top loader is not visible when i go to another page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { PageLoader } from '@/components/page-loader';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 export const metadata: Metadata = {
   title: 'BizHome',
@@ -22,7 +22,12 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@400;700&family=IBM+Plex+Sans:wght@400;700&family=Inter:wght@400;700&family=Poppins:wght@400;700&family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <PageLoader />
+        <ProgressBar
+          height="4px"
+          color="#FFD139"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
         {children}
         <Toaster />
       </body>
