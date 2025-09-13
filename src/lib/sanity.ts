e3da24553c77e5e5ffa5c6515f0beef0e2593994
Venkateshwar <venkattiwari42@gmail.com>
2025-09-13 15:38:53 +0000

    i get a circular loader and this error when openig cart page in mobile

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,9 +1,10 @@
+
 // @/lib/sanity.ts
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'yu413ogt', // REPLACE WITH YOUR SANITY PROJECT ID
-  dataset: 'production', // REPLACE WITH YOUR SANITY DATASET
+  projectId: 'yu413ogt',
+  dataset: 'production',
   apiVersion: '2024-01-01',
   useCdn: true,
 });
