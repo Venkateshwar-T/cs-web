a1846a18e6c2eda5f92d8341795ff93d5462c916
Venkateshwar <venkattiwari42@gmail.com>
2025-09-13 15:33:30 +0000

    Error: Request error while attempting to reach is https://yu413ogt.apicd

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: 'yu413ogt', // REPLACE WITH YOUR SANITY PROJECT ID
+  dataset: 'production', // REPLACE WITH YOUR SANITY DATASET
   apiVersion: '2024-01-01',
   useCdn: true,
 });
