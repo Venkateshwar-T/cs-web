f59d9d03e5599e74234c3adba5bb970b9e697fb0
Venkateshwar <venkattiwari42@gmail.com>
2025-09-21 11:40:22 +0000

    Fixed Sign In

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,12 +12,12 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AI_ADC_MARKER_FIREBASE_API_KEY",
-  authDomain: "AI_ADC_MARKER_FIREBASE_AUTH_DOMAIN",
-  projectId: "AI_ADC_MARKER_FIREBASE_PROJECT_ID",
-  storageBucket: "AI_ADC_MARKER_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "AI_ADC_MARKER_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "AI_ADC_MARKER_FIREBASE_APP_ID"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
