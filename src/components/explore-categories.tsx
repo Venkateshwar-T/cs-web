72800be9979d6e2bcb9cf38b2b8a7940c77a8f9c
Venkateshwar <venkattiwari42@gmail.com>
2025-09-21 16:23:35 +0000

    Noting happened

--- src/components/explore-categories.tsx
+++ src/components/explore-categories.tsx
@@ -47,14 +47,14 @@ export function ExploreCategories() {
 
   return (
     <motion.div 
-      className="bg-[#5D2B79] h-full rounded-t-[20px] md:rounded-t-[40px] px-4 md:px-0 md:mx-32"
+      className="bg-[#5D2B79] h-full rounded-t-[20px] md:rounded-t-[40px] mx-4 md:mx-32"
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ type: 'spring', stiffness: 80, damping: 15 }}
     >
         <div className="bg-white/20 h-full rounded-t-[25px] md:rounded-t-[40px] py-4 md:py-6 px-4 md:px-12 flex flex-col">
             <div className="flex-grow overflow-y-auto no-scrollbar min-h-0">
-                <SectionTitle className="flex justify-center md:justify-start pl-2 text-lg md:text-2xl md:pl-8 mb-2 md:mb-4">
+                <SectionTitle className="flex justify-center md:justify-start text-lg md:text-2xl md:pl-8 mb-2 md:mb-4">
                     Explore Categories
                 </SectionTitle>
                 <motion.div 
