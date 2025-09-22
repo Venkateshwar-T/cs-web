ec8eaa2c34d960f39777cebc4368f17840a80108
Venkateshwar <venkattiwari42@gmail.com>
2025-09-22 15:40:15 +0000

    good. Now also increase the side padding to the inner contents

--- src/components/product-order-details-popup.tsx
+++ src/components/product-order-details-popup.tsx
@@ -1,3 +1,4 @@
+
 // @/components/product-order-details-popup.tsx
 'use client';
 
@@ -78,7 +79,7 @@ export function ProductOrderDetailsPopup({ details, open, onOpenChange, onViewPr
             <span className="sr-only">Close</span>
           </DialogClose>
         </DialogHeader>
-        <div className="flex flex-col gap-4 px-4 pt-8 pb-8 text-white max-h-[80vh]">
+        <div className="flex flex-col gap-4 px-6 pt-8 pb-8 text-white max-h-[80vh]">
             <div className="flex flex-col md:flex-row items-center md:items-start gap-4">
                 <div className="w-2/5 md:w-1/3 flex-shrink-0">
                     <Image
