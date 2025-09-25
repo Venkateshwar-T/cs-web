6b02db4f81303e500eea4f5ab68f6290b3b449ab
Venkateshwar <venkattiwari42@gmail.com>
2025-09-25 12:22:32 +0000

    you made the change but the toast still stays for like 3-4 seconds and t

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,3 +1,4 @@
+
 "use client"
 
 // Inspired by react-hot-toast library
@@ -158,6 +159,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
+      duration: 1000,
       onOpenChange: (open) => {
         if (!open) dismiss()
       },
